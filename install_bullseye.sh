b8241b84b9bfd300dc184b12a35d01c06f9c092d
Daniel Hafner <daniel.hafner@holidaycheck.com>
2024-09-13 22:26:54 +0200

    install script

--- install_bullseye.sh
+++ install_bullseye.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-function exitcodefunction {
-  errorcode=$1
+function exit_code_function {
+  error_code=$1
   action=$2
   module=$3
 
-  if [ $errorcode -ne "0" ]; then
+  if [ "$error_code" -ne "0" ]; then
     echo "Action: $action on $module failed." >> $boswatch_install_path/setup_log.txt
-    echo "Exitcode: $errorcode" >> $boswatch_install_path/setup_log.txt
+    echo "Exitcode: $error_code" >> $boswatch_install_path/setup_log.txt
     echo ""
     echo "Action: $action on $module failed."
-    echo "Exitcode: $errorcode"
+    echo "Exitcode: $error_code"
     echo ""
     echo " -> If you want to open an issue at https://github.com/Schrolli91/BOSWatch/issues"
     echo "    please post the logfile, located at $boswatch_install_path/setup_log.txt"
@@ -43,37 +43,37 @@ echo ""
 echo "Caution, script does not install a webserver with PHP and MySQL"
 echo "So you have to make up manually if you want to use MySQL support"
 
-boswatchpath=/opt/boswatch
+boswatch_path=/opt/boswatch
 boswatch_install_path=/opt/boswatch_install
 reboot=false
 didBackup=false
 
 # Checking for Backup
 # check for old version (for the old ones...)
-if [ -f $boswatchpath/BOSWatch/boswatch.py ]; then
+if [ -f $boswatch_path/BOSWatch/boswatch.py ]; then
 	echo "Old installation found!"
-	echo "A backup will be copied to $boswatchpath/old"
+	echo "A backup will be copied to $boswatch_path/old"
 
 	mkdir /tmp/boswatch
-	mv $boswatchpath/BOSWatch/* /tmp/boswatch/
+	mv $boswatch_path/BOSWatch/* /tmp/boswatch/
 	didBackup=true
 fi
 
 #and the future...
-if [ -f $boswatchpath/boswatch.py ]; then
+if [ -f $boswatch_path/boswatch.py ]; then
 	echo "Old installation found!"
-	echo "A backup will be copied to $boswatchpath/old"
+	echo "A backup will be copied to $boswatch_path/old"
 
 	mkdir /tmp/boswatch
-	mv $boswatchpath/* /tmp/boswatch/
+	mv $boswatch_path/* /tmp/boswatch/
 	didBackup=true
 fi
 
 # Check for Flags in command line
-for (( i=1; i<=$#; i=$i+2 )); do
+for (( i=1; i <= $#; i+=2 )); do
+    arg=${!i}
     t=$((i + 1))
-    eval arg=\$$i
-    eval arg2=\$$t
+    arg2=${!t}
 
     case $arg in
       -r|--reboot) reboot=true ;;
@@ -84,15 +84,15 @@ for (( i=1; i<=$#; i=$i+2 )); do
         *) branch=master ;;
       esac ;;
 
-      -p|--path)    echo " !!! WARNING: you'll install BOSWATCH to alternative path !!! "; boswatchpath=$arg2 ;;
+      -p|--path)    echo " !!! WARNING: you'll install BOSWATCH to alternative path !!! "; boswatch_path=$arg2 ;;
 
       *) echo "Internal error!" ; exit 1 ;;
     esac
 done
 
 # Create default paths
-mkdir -p $boswatchpath
-mkdir -p $boswatch_install_path
+mkdir -p "$boswatch_path"
+mkdir -p "$boswatch_install_path"
 
 echo ""
 
@@ -108,29 +108,22 @@ tput cup 13 15
 echo "[ 2/9] [##-------]"
 tput cup 15 5
 echo "-> download GIT and other stuff.........."
-apt-get -y install git python cmake build-essential libusb-1.0 qt5-qmake qtbase5-dev libpulse-dev libx11-dev sox  >> \
+apt-get -y install git cmake build-essential libusb-1.0 qt5-qmake qtbase5-dev libpulse-dev libx11-dev sox python3-pip >> \
 $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? download stuff
-
-# download and install pip2
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py >> $boswatch_install_path/setup_log.txt 2>&1 
-exitcodefunction $? curl pip2
-python2 get-pip.py >> $boswatch_install_path/setup_log.txt 2>&1 exitcodefunction $? python2 get-pip
-rm get-pip.py
-
+exit_code_function $? download stuff
 
 # download BOSWatch via git
 tput cup 13 15
 echo "[ 3/9] [###------]"
 tput cup 15 5
 echo "-> download BOSWatch..................."
-cd $boswatchpath/
+cd "$boswatch_path/" || { echo "Failed to change directory to $boswatch_path. Exiting."; exit 1; }
 
 case $branch in
-  "dev") git clone -b develop https://github.com/Schrolli91/BOSWatch . >> $boswatch_install_path/setup_log.txt 2>&1 && \
-    exitcodefunction $? git-clone BOSWatch-develop ;;
-  *) git clone -b master https://github.com/Schrolli91/BOSWatch . >> $boswatch_install_path/setup_log.txt 2>&1 && \
-    exitcodefunction $? git-clone BOSWatch ;;
+  "dev") git clone -b develop https://github.com/daniel1608/BOSWatch . >> $boswatch_install_path/setup_log.txt 2>&1 && \
+    exit_code_function $? git-clone BOSWatch-develop ;;
+  *) git clone -b master https://github.com/daniel1608/BOSWatch . >> $boswatch_install_path/setup_log.txt 2>&1 && \
+    exit_code_function $? git-clone BOSWatch ;;
 esac
 
 # Download RTL-SDR
@@ -138,28 +131,30 @@ tput cup 13 15
 echo "[ 4/9] [####-----]"
 tput cup 15 5
 echo "-> download rtl_fm......................"
-cd $boswatch_install_path
+cd "$boswatch_install_path" || { echo "Failed to change directory to $boswatch_install_path. Exiting."; exit 1; }
 git clone https://github.com/Schrolli91/rtl-sdr.git >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? git-clone rtl-sdr
-cd rtl-sdr/
+exit_code_function $? git-clone rtl-sdr
+cd "rtl-sdr/" || { echo "Failed to change directory to rtl-sdr. Exiting."; exit 1; }
 
 # Compie RTL-FM
 tput cup 13 15
 echo "[ 5/9] [#####----]"
 tput cup 15 5
 echo "-> compile rtl_fm......................"
-mkdir -p build && cd build
+mkdir -p build || { echo "Failed to create directory 'build'. Exiting."; exit 1; }
+cd build || { echo "Failed to change directory to 'build'. Exiting."; exit 1; }
+
 cmake ../ -DINSTALL_UDEV_RULES=ON >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? cmake rtl-sdr
+exit_code_function $? cmake rtl-sdr
 
 make >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? make rtl-sdr
+exit_code_function $? make rtl-sdr
 
 make install >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? make-install rtl-sdr
+exit_code_function $? make-install rtl-sdr
 
 ldconfig >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? ldconfig rtl-sdr
+exit_code_function $? ldconfig rtl-sdr
 
 
 # Download Multimon-NG
@@ -167,11 +162,11 @@ tput cup 13 15
 echo "[ 6/9] [######---]"
 tput cup 15 5
 echo "-> download multimon-ng................"
-cd $boswatch_install_path
+cd "$boswatch_install_path" || { echo "Failed to change directory to $boswatch_install_path. Exiting."; exit 1; }
 git clone https://github.com/Schrolli91/multimon-ng.git multimonNG >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? git-clone multimonNG
+exit_code_function $? git-clone multimonNG
 
-cd $boswatch_install_path/multimonNG/
+cd "$boswatch_install_path/multimonNG/" || { echo "Failed to change directory to $boswatch_install_path/multimonNG/. Exiting."; exit 1; }
 
 # Compile Multimon-NG
 tput cup 13 15
@@ -179,58 +174,58 @@ echo "[ 7/9] [#######--]"
 tput cup 15 5
 echo "-> compile multimon-ng................."
 mkdir -p build
-cd build
+cd build || { echo "Failed to change directory to build. Exiting."; exit 1; }
 qmake ../multimon-ng.pro >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? qmake multimonNG
+exit_code_function $? qmake multimonNG
 
 make >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? make multimonNG
+exit_code_function $? make multimonNG
 
 make install >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? qmakeinstall multimonNG
+exit_code_function $? qmakeinstall multimonNG
 
 # Download & Install MySQL-Connector for Python via pip
 tput cup 13 15
 echo "[ 8/9] [########-]"
 tput cup 15 5
 echo "-> Download & Install MySQL connector for Python."
-cd $boswatch_install_path
-pip install mysql-connector-python >> $boswatch_install_path/setup_log.txt 2>&1
-exitcodefunction $? install mysql-connector
+cd "$boswatch_install_path" || { echo "Failed to change directory to $boswatch_install_path. Exiting."; exit 1; }
+pip3 install mysql-connector-python >> $boswatch_install_path/setup_log.txt 2>&1
+exit_code_function $? install mysql-connector
 
 # Blacklist DVB-Drivers
 tput cup 13 15
 echo "[9/9] [#########]"
 tput cup 15 5
 echo "-> configure..........................."
-cd $boswatchpath/
+cd "$boswatch_path/" || { echo "Failed to change directory to $boswatch_path. Exiting."; exit 1; }
 echo $'# BOSWatch - blacklist the DVB drivers to avoid conflicts with the SDR driver\n blacklist dvb_usb_rtl28xxu \n blacklist rtl2830\n blacklist dvb_usb_v2\n blacklist dvb_core' >> /etc/modprobe.d/boswatch_blacklist_sdr.conf
 
 # Installation is ready
 tput cup 17 1
-echo "BOSWatch is now installed in $boswatchpath/"
+echo "BOSWatch is now installed in $boswatch_path/"
 echo "Installation ready!"
 tput cup 19 3
 echo "Watch out: to run BOSWatch you have to modify the config.ini!"
 echo "Do the following step to do so:"
-echo "sudo nano $boswatchpath/config/config.ini"
+echo "sudo nano $boswatch_path/config/config.ini"
 echo "and modify the config as you need. This step is optional if you are upgrading an old version of BOSWatch. "
 
 tput cnorm
 
 # cleanup
-mkdir $boswatchpath/log/install -p
-mv $boswatch_install_path/setup_log.txt $boswatchpath/log/install/
-rm $boswatch_install_path/ -R
+mkdir "$boswatch_path"/log/install -p
+mv $boswatch_install_path/setup_log.txt "$boswatch_path"/log/install/
+rm -R "${boswatch_install_path:?}/"
 
 #copy the template config to run boswatch
-cp $boswatchpath/config/config.template.ini $boswatchpath/config/config.ini
+cp "$boswatch_path"/config/config.template.ini "$boswatch_path"/config/config.ini
 
 
 #replay the backup
 if [ $didBackup = "true" ]; then
-	mkdir $boswatchpath/old/
-	mv /tmp/boswatch/* $boswatchpath/old/
+	mkdir "$boswatch_path"/old/
+	mv /tmp/boswatch/* "$boswatch_path"/old/
 fi
 
 if [ $reboot = "true" ]; then
